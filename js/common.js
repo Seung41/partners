a69d50fc9e6af116be927320881ff744ed90a169
Seung41 <seung9441@naver.com>
2019-11-29 17:15:32 +0900

    191129 반응형 레이아웃 완성

--- js/common.js
+++ js/common.js
@@ -11,8 +11,18 @@ $(document).ready(function () {
         $("#main_view").load("/html/partners_admin_board.html");
     });
     
-    
+    $("#bd_list_tbl_select tbody tr").hover(function(){
 
+        $(this).css('background', '#d64843');
+    });
+    
+    $("#bd_list_tbl_select tbody tr").mouseleave(function(){
+        $(this).css('background', '#4a4742');
+    });
+
+    $("#bd_list_tbl_select tbody tr").click(function(){
+        $("#main_view").load("/html/partners_admin_board_edit.html");
+    });
     //$(".sideBar_menu li").each(function(){
     //    console.log($(this).text());
     //});
